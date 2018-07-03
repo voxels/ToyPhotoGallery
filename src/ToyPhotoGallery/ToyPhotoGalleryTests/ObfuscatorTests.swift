d7e2a01a1a50501cf4e4e4c8956459dbd6a76130
Michael Edgcumbe <voxels@gmail.com>
2018-07-03 11:27:55 -0400

    Adding Obfuscator class tests

--- src/ToyPhotoGallery/ToyPhotoGalleryTests/ObfuscatorTests.swift
+++ src/ToyPhotoGallery/ToyPhotoGalleryTests/ObfuscatorTests.swift
@@ -7,22 +7,24 @@
 //
 
 import XCTest
+@testable import ToyPhotoGallery
 
 class ObfuscatorTests: XCTestCase {
-    
-    func testInitSetsExpectedSalt() {
-        
-    }
-    
+    let salt = [NSString.self, NSSet.self]
+
     func testBytesByObfuscatingStringReturnsExpectedBytes() {
-        
+        let testString = "HelloWorld"
+        let expected:[UInt8] = [19, 43, 63, 63, 27, 37, 6, 28, 11, 72]
+        let obfuscator = Obfuscator(withSalt: salt)
+        let actual = obfuscator.bytesByObfuscatingString(testString)
+        XCTAssertEqual(expected, actual)
     }
     
     func testRevealReturnsExpectedString() {
-        
+        let expected = "HelloWorld"
+        let testBytes:[UInt8] = [19, 43, 63, 63, 27, 37, 6, 28, 11, 72]
+        let obfuscator = Obfuscator(withSalt: salt)
+        let actual = obfuscator.reveal(testBytes)
+        XCTAssertEqual(expected, actual)
     }
-    
-    func testSaltObjectsReturnsExpectedObjects() {
-        
-    }    
 }
